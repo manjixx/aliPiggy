fd7013ea2c833aec18dd395d0208b7067a4da563
manjixx <xiaochuiwei@gmail.com>
2023-12-10 23:48:02 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# PiggyMetric-Alibaba Cloud
+
+## 项目概述
+
+本项目是基于 [piggyMetris](https://github.com/sqshq/piggymetrics)利用alibaba cloud 版本的实现
+
+### 技术选型
+
+
+| 技术栈   | 原项目技术选型  | 本项目技术选型  |
+|---|---|---|
+|API网关|Spring Cloud Zuul| Spring Cloud Gateway|
+|负载均衡|Eurake+Ribbon | Ribbon |
+|远程调用|Feign|Open Feign|
+|限流熔断|Hystrix| Sentinel |
+|服务注册与发现|Spring Cloud Eurake|Nacos|
+|授权认证|Spring Cloud OAuth2| Spring Cloud OAuth2 |
+|配置中心|pring Cloud Config&&Apollo| Nacos |
+|分布式调用链监控|Spring Cloud Sleuth| SkyWalking|
+|链路追踪|Zipkin|SkyWalking|
+|消息队列|RabbitMQ|RocketMQ|
+|日志监控|ELK|ELK |
+
---
+++ account-service/src/main/java/com/alipiggy/account/contorller/ConfigController.java
@@ -0,0 +1,25 @@
+package com.alipiggy.account.contorller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author：
+ * Date：2023/12/1022:37
+ * Desc:
+ */
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
